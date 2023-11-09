6776cbcae45d2240b62174c6de6738a0952b4ffe
thelssa <141613467+thelssa@users.noreply.github.com>
2023-11-09 15:57:19 +0100

    Thelma_backtracking.R

--- Thelma_backtracking.R
+++ Thelma_backtracking.R
@@ -17,36 +17,40 @@ backtracking <- function(x, y) {
   }
   
   placer_dames <- function(cb, colonne = 1) {
-    if (colonne > x) {
-      k <<- k + 1
-      print(cb)
-    } else {
+    while (colonne <= x) {
       ligne <- 1
       while (ligne <= y) {  # Utilisez y pour le nombre de colonnes
         if (est_valide(cb, colonne, ligne)) {
           cb[colonne] <- ligne
-          placer_dames(cb, colonne + 1)
+          if (colonne == x) {
+            k <<- k + 1
+            print(cb)
+          } else {
+            placer_dames(cb, colonne + 1)
+          }
         }
         ligne <- ligne + 1
       }
+      colonne <- colonne + 1
     }
   }
   
   placer_dames(cb_initial)
-  cat("Nombre de k trouvées:", k, "\n") #Affiche le nombre total de solutions trouvées.
-  
+  cat("Nombre de k trouvées:", k, "\n") # Affiche le nombre total de solutions trouvées.
   
   # Création du heatmap pour visualiser les cases les plus touchées
   
   plateau <- matrix(0, nrow = x, ncol = y) # Création de la matrice de dimensions x par y remplie de zéros
-  for (col in 1:x) {
+  col <- 1
+  while (col <= x) {
     row <- cb_initial[col]
     plateau[row, col] <- plateau[row, col] + 1
-    for (i in 1:x) {
+    i <- 1
+    while (i <= x) {
       if (i != col) {
         # Attaques horizontales et diagonales
         
-        plateau[cb_initial[i], col] <- plateau[cb_initial[i], col] + 1 #Incrémente le nombre d'attaques sur la case correspondante dans la matrice
+        plateau[cb_initial[i], col] <- plateau[cb_initial[i], col] + 1 # Incrémente le nombre d'attaques sur la case correspondante dans la matrice
         
         if (cb_initial[i] + (col - i) > 0 && cb_initial[i] + (col - i) <= y) {
           plateau[cb_initial[i] + (col - i), col] <- plateau[cb_initial[i] + (col - i), col] + 1
@@ -55,10 +59,12 @@ backtracking <- function(x, y) {
           plateau[cb_initial[i] - (col - i), col] <- plateau[cb_initial[i] - (col - i), col] + 1
         }
       }
+      i <- i + 1
     }
+    col <- col + 1
   }
   
-  #Création du heatmap avec la bibliothèque gplots
+  # Création du heatmap avec la bibliothèque gplots
   
   heatmap(as.matrix(plateau), Rowv = NA, Colv = NA, col = heat.colors(12), scale = "column",
           main = "Backtracking", xlab = "Colonnes", ylab = "Lignes",
@@ -66,7 +72,5 @@ backtracking <- function(x, y) {
 }
 
 # Lancement de la fonction principale
-backtracking(4, 4)
-
-
+backtracking(5,5)
 
